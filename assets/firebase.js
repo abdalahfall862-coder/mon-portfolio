99f41330d7d6f1117a986b3dae0ad42e499c3149
Mohamed Abdalah Fall <abdalahfall@gmail.com>
2026-04-23 16:52:52 +0000

    refactor: clean up firebase.js and improve index.html structure

--- assets/firebase.js
+++ assets/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyDiuBAsoGDu2L-K2m1rI1-UIVmoXM5uJKc",
   authDomain: "my-portfolio-6f2b6.firebaseapp.com",
@@ -16,6 +12,5 @@ const firebaseConfig = {
   measurementId: "G-0VQ997N1V0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
